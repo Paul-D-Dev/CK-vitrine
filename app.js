b2a4220161fe8edb628aed9c6f389045ae616eb5
Paul DAO <dao.van.paul@gmail.com>
2020-09-23 17:23:32 +0200

    CSS responsive

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const burgerBtn = document.getElementById('burger-menu');
 const header = document.getElementById('header');
+const body = document.getElementById('body');
+
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('open');
     header.classList.toggle('open');
+    body.classList.toggle('unscroll')
 })
