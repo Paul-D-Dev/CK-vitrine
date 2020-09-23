5bd6c77fc4665e8b3fcc8392b95f23f54945b01c
Paul DAO <dao.van.paul@gmail.com>
2020-09-23 18:38:19 +0200

    JS handle active link on click

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ burgerBtn.addEventListener('click', () => {
     header.classList.toggle('open');
     body.classList.toggle('unscroll')
 })
+
+
+// handle active in header
+const links = document.getElementsByClassName('link');
+
+function removeActive() {
+    const current = document.getElementsByClassName('active');
+    current[0].classList.remove('active');
+}
+
+for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    link.addEventListener('click', () => {
+        removeActive();
+        link.classList.add('active');
+    })
+}
+
+// if click on Author, ACCUEIL is active;
+const author = document.getElementsByClassName('header__nav__ck');
+author[0].addEventListener('click', () => {
+    if(!links[0].classList.contains('active')) {
+        removeActive();
+        links[0].classList.add('active');
+    }
+})
